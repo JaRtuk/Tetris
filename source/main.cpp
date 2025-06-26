b57487ec86f4bd1ec765b911a1b1fff90511827b
JaRtuk <elden.aa6001@yahoo.com>
2025-06-26 19:58:39 +0300

    Добавлена библиотека glad

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,7 @@
-#include "../external/glfw/include/GLFW/glfw3.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -9,7 +12,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Tetris ultima", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -19,11 +22,21 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGL())
+    {
+        std::cout << "Cant load Glad!" << std::endl;
+        return -1;
+    }
+
+    std::cout << "OpenGL" << GLVersion.major << '.' << GLVersion.minor << std::endl;
+
+    glClearColor(255.f, 255.f, 0.f, 255.f);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
